0cb5b33365aa46c50c575b172a79ccd4e979a2c1
Peter Joshua Brion <37060599+peterbrionibinex@users.noreply.github.com>
2018-03-08 16:43:00 +0800

    Create GeneralElementsExercise_PJ.php

---
+++ GeneralElementsExercise_PJ.php
@@ -0,0 +1,27 @@
+<?php 
+    
+    // PHP is a weakly-type programming language. No need to declare the "type" of the value of your variable
+    // after putting 2 forward slash it becames comment
+    # single line commented code
+    /* multi line comment */
+    
+    $firstVariable = "Our first Variable\n"; //\n used for carriage return or new line
+    $firstvariable = "Really is it our first]\n";
+    
+    echo $firstVariable;
+    echo $firstvariable;
+    
+    class Company{
+        
+    }
+    
+    $p1 = Company;
+    $p2 = COMPANY;
+    
+    function website(){
+        echo "http://pluralsight.com\n";
+    }
+    
+    webSite();
+    Website();
+    WEBSITE();
