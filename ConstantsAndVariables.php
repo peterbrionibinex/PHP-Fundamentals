265b74ceb3a2308e3692b136f1fff870cc13d512
Peter Joshua Brion <37060599+peterbrionibinex@users.noreply.github.com>
2018-03-08 17:12:32 +0800

    added ConstantsAndVariables file

---
+++ ConstantsAndVariables.php
@@ -0,0 +1,10 @@
+<?php
+    /*Constants and Variables*/
+    
+    // Only scalar values can be constants (e.g. Boolean, integer, double, and string
+    
+    define("NEW_CONSTANT", "Hello New Constant!");
+    
+    echo NEW_CONSTANT."\n";
+    
+    var_dump(NEW_CONSTANT);
