37a2d33578fb916c69eede74f948d9e043d0dca1
Peter Joshua Brion <37060599+peterbrionibinex@users.noreply.github.com>
2018-03-09 13:20:10 +0800

    Update ConstantsAndVariables.php

--- PHP Fundamentals/ConstantsAndVariables.php
+++ PHP Fundamentals/ConstantsAndVariables.php
@@ -3,8 +3,8 @@
     
     // Only scalar values can be constants (e.g. Boolean, integer, double, and string
     
-    define("NEW_CONSTANT", "Hello New Constant!");
+    define("NEW_CONSTANT", "Hello New Constant!"); //Defines new constant "NEW_CONSTANT" with the value of "Hello New Constant"
     
-    echo NEW_CONSTANT."\n";
+    echo NEW_CONSTANT."\n"; //prints the value of NEW_CONSTANT
     
-    var_dump(NEW_CONSTANT);
+    var_dump(NEW_CONSTANT); //checks the datatype of NEW_CONSTANT
