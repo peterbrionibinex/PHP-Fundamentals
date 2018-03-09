49b9efebb4670391e5aedb096de62e3cf1b195df
Peter Joshua Brion <37060599+peterbrionibinex@users.noreply.github.com>
2018-03-09 11:52:09 +0800

    Update HelloWorldProgramHTML.php

--- HelloWorldProgramHTML.php
+++ HelloWorldProgramHTML.php
@@ -1,12 +1,16 @@
 <?php
-    $message = "Hello Shiro"; 
+    /* 
+        This PHP program contains the $message and $title variable 
+        in which it will be passed on to HTML title tag and
+        HTML body tag.
+    */
+    $message = "Hello World"; 
     $title = "HTML Sample";
 ?>
 
 <!DOCTYPE html>
 <html>
-    <head><title>PHP Fundamentals - <?=$title?></title></head> // prints
-    <body><h1><?php echo $message?></h1></body>
+    <head><title>PHP Fundamentals - <?=$title?></title></head> #prints PHP Fundamentals - Hello World
+    <body><h1><?php echo $message?></h1></body> #prints HTML Sample
 </html>
 
-/* This exercise was made by Jill Gurensen and made some modifications by me.*/
