6d8e5aea83a9d2fba6f0e4745cae9e3b90d3c3cd
Peter Joshua Brion <37060599+peterbrionibinex@users.noreply.github.com>
2018-03-09 11:41:44 +0800

    Update helloworld.php

--- helloworld.php
+++ helloworld.php
@@ -1,6 +1,8 @@
 <?php
-    $message = "Hello World!";
+    $message = "Hello World!"; //declaring variable $message
     
-    echo $message;
+    echo $message; //prints the content of $message variable
+    
 
+    /* This exercise was made by Jill Gurensen and made some modifications by me.*/
 ?>
